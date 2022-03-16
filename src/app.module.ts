969fd297f8e0e12ac78bb5b4c62964ab3a369b9c
kimsijun <papaya9349@naver.com>
2022-03-16 23:36:11 +0900

    사용자 인증 메일 발송, TypeORM 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,43 @@
-import { Module } from '@nestjs/common';
-import { MoviesModule } from './movies/movies.module';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationScheme } from './config/validationSchema';
+import { MoviesModule } from './movies/movies.module';
 import { UserModule } from './user/user.module';
+import { EmailModule } from './email/email.module';
+import { SubModule } from './sub/sub.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './app.guard';
+
+const configModule = ConfigModule.forRoot({
+  envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+  load: [emailConfig],
+  isGlobal: true,
+});
 
 @Module({
-  imports: [MoviesModule, UserModule, TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot(),
+    configModule,
+    MoviesModule,
+    UserModule,
+    EmailModule,
+    SubModule,
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('/user');
+  }
+}
